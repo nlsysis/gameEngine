92d91e05ab29abcdc3d6310ebda72817fddceb59
nlsysis <nlsysis@126.com>
2020-10-26 15:46:38 +0900

    No.54 2 in 1 shader
    used whiteTexture trick

--- gameEngineTest/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ gameEngineTest/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -28,5 +28,6 @@ namespace Engine
 	void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray)
 	{
 		glDrawElements(GL_TRIANGLES, vertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 }
--- gameEngineTest/src/Platform/OpenGL/OpenGLTexture.cpp
+++ gameEngineTest/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -3,10 +3,20 @@
 
 #include "stb_image.h"
 
-#include <glad/glad.h>
-
 namespace Engine
 {
+	OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height)
+		:m_Width(width), m_Height(height)
+	{
+		m_InternalFormat = GL_RGBA8;
+		m_DataFormat = GL_RGBA;
+
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_RenderID);
+		glTextureStorage2D(m_RenderID, 1, m_InternalFormat, m_Width, m_Height);
+
+		glTextureParameteri(m_RenderID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTextureParameteri(m_RenderID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	}
 
 	OpenGLTexture2D::OpenGLTexture2D(const std::string& path)
 		:m_Path(path)
@@ -30,6 +40,8 @@ namespace Engine
 			dataFormat = GL_RGB;
 		}
 
+		m_InternalFormat = internalFormat;
+		m_DataFormat = dataFormat;
 
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_RenderID);
 		glTextureStorage2D(m_RenderID, 1, internalFormat, m_Width, m_Height);
@@ -45,6 +57,13 @@ namespace Engine
 	OpenGLTexture2D::~OpenGLTexture2D()
 	{
 		glDeleteTextures(1, &m_RenderID);
+	}
+
+	void OpenGLTexture2D::SetData(void* data, uint32_t size)
+	{
+		uint32_t bpp = m_DataFormat == GL_RGBA ? 4 : 3;
+		EG_CORE_ASSERT((size == m_Width * m_Height * bpp), "Data must be entire texture!")
+		glTextureSubImage2D(m_RenderID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
 	}
 
 	void OpenGLTexture2D::Bind(uint32_t slot) const
